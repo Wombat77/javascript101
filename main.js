b10720afc725f0bb6df1bc3ccaaa5ff40b32e5c3
Fendt <csar2168@uibk.ac.at>
2018-04-11 17:29:56 +0200

    Version1

---
+++ main.js
@@ -0,0 +1,27 @@
+alert("Hello World");
+
+let nachricht = "Hallo Welt";
+//alert(nachricht);
+nachricht = "was geht denn aab";
+//alert(nachricht);
+console.log(nachricht);
+const linkColor = "#ff0000";
+let eine_variable = "test";
+console.log("Variable: eine_variable: ", eine_variable);
+let highScore = 8349087;
+console.log("Highscore ist: ", highScore / 20);
+let meineNachricht = "test1";
+console.log(meineNachricht);
+meineNachricht = "test2";
+console.log(meineNachricht);
+meineNachricht = "Ein mehrstufiges Haus liegt am meeresbusen der Adria und sieht gut aus";
+
+console.log(meineNachricht);
+
+let fullName = `Jeffrey Schnuppinger Bigbowsky`;
+console.log(fullName);
+
+let isOver18 = true
+console.log("über 18?", isOver18);
+let age = 19;
+console.log("age über 18", age > 18);
